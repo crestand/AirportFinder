d84953fbe433f52fecf34b898099a974196b69f9
crestand <eren.h.koc@gmail.com>
2024-08-07 12:11:13 +0300

    feat(APF-2): Created SearchCityVC base

---
+++ AirportFinder/ViewModel/SearchCityViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCityViewModel.swift
+//  AirportFinder
+//
+//  Created by Eren Koç on 6.08.2024.
+//
+
+protocol SearchCityViewPresentable {
+    
+}
+
+class SearchCityViewModel: SearchCityViewPresentable{
+    
+}
